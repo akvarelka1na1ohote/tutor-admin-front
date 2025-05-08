7a59aa982a6574c3ec6b6789d86bddc38ba5c2e8
Kostin Pavel <50001467+KostinP@users.noreply.github.com>
2025-05-08 18:28:23 +0300

    update

--- src/pages/Search_client.jsx
+++ src/pages/Search_client.jsx
@@ -10,6 +10,7 @@ import Price from '../components/filters/tutor/price';
 import Place from '../components/filters/tutor/place';
 import Location from '../components/filters/tutor/location';
 import Timetable from '../components/filters/tutor/timetable';
+import Form from '../components/filters/form';
 import Footer from '../components/footer/footer';
 import SearchSwitchButtonClient from '../components/buttons/changeFromClientSearch.jsx';
 import { Api } from '../Api';
@@ -28,6 +29,7 @@ const SearchPageClient = () => {
   const [place, setPlace] = useState([]);
   const [location, setLocation] = useState([]);
   const [timetable, setTimetable] = useState([]);
+  const [role, setRole] = useState(null);
   const [performers, setPerformers] = useState([]);
 
   // Fetch subjects on mount
@@ -59,7 +61,6 @@ const SearchPageClient = () => {
       if (selectedSubject) {
         query.subjectId = selectedSubject.id;
       }
-      // Add other filters as query params (e.g., age, gender, etc.)
       if (ageRange.from) query.age_From = parseInt(ageRange.from);
       if (ageRange.to) query.age_To = parseInt(ageRange.to);
       if (priceRange.from) query.cost_From = parseInt(priceRange.from);
@@ -87,8 +88,25 @@ const SearchPageClient = () => {
       if (selectedCourse) {
         query[`course_${selectedCourse.id}`] = true;
       }
+      if (timetable.length > 0) {
+        timetable.forEach((day) => {
+          const dayMap = {
+            'По договоренности': 'arrangement',
+            'Понедельник': 'monday',
+            'Вторник': 'tuesday',
+            'Среда': 'wednesday',
+            'Четверг': 'thursday',
+            'Пятница': 'friday',
+            'Суббота': 'saturday',
+            'Воскресенье': 'sunday'
+          };
+          if (dayMap[day]) query[dayMap[day]] = true;
+        });
+      }
+      if (role !== null) {
+        query.id_Role = role;
+      }
 
-      // Fetch performers (you may need a custom endpoint for filtered search)
       const response = await api.api.performersList({ query });
       setPerformers(response.data);
     } catch (error) {
@@ -99,23 +117,34 @@ const SearchPageClient = () => {
   // Fetch performers whenever filters change
   useEffect(() => {
     fetchPerformers();
-  }, [selectedSubject, selectedClass, selectedCourse, gender, ageRange, experience, priceRange, place, location, timetable]);
+  }, [selectedSubject, selectedClass, selectedCourse, gender, ageRange, experience, priceRange, place, location, timetable, role]);
 
   return (
     <>
       <SearchSwitchButtonClient />
-      <div className='bg-white w-[300px] h-[1550px] mt-[30px] ml-[120px] mb-[200px] rounded-lg relative'>
-        <Role />
-        <Items subjects={subjects} selectedSubject={selectedSubject} setSelectedSubject={setSelectedSubject} />
-        <Class selectedClass={selectedClass} setSelectedClass={setSelectedClass} />
-        <Gender gender={gender} setGender={setGender} />
-        <Age ageRange={ageRange} setAgeRange={setAgeRange} />
-        <Courses selectedCourse={selectedCourse} setSelectedCourse={setSelectedCourse} />
-        <Experience experience={experience} setExperience={setExperience} />
-        <Price priceRange={priceRange} setPriceRange={setPriceRange} />
-        <Place place={place} setPlace={setPlace} />
-        <Location location={location} setLocation={setLocation} />
-        <Timetable timetable={timetable} setTimetable={setTimetable} />
+      <div className="flex">
+        <div className="bg-white w-[300px] h-[1550px] mt-[30px] ml-[120px] mb-[200px] rounded-lg relative">
+          <Role role={role} setRole={setRole} />
+          <Items subjects={subjects} selectedSubject={selectedSubject} setSelectedSubject={setSelectedSubject} />
+          <Class selectedClass={selectedClass} setSelectedClass={setSelectedClass} />
+          <Gender gender={gender} setGender={setGender} />
+          <Age ageRange={ageRange} setAgeRange={setAgeRange} />
+          <Courses selectedCourse={selectedCourse} setSelectedCourse={setSelectedCourse} />
+          <Experience experience={experience} setExperience={setExperience} />
+          <Price priceRange={priceRange} setPriceRange={setPriceRange} />
+          <Place place={place} setPlace={setPlace} />
+          <Location location={location} setLocation={setLocation} />
+          <Timetable timetable={timetable} setTimetable={setTimetable} />
+        </div>
+        <div className="mt-[30px] ml-[50px] absolute left-[370px] top-[200px]">
+          {performers.length > 0 ? (
+            performers.map((performer) => (
+              <Form key={performer.id} item={performer} type="performer" />
+            ))
+          ) : (
+            <p className="text-gray-600">Репетиторы не найдены</p>
+          )}
+        </div>
       </div>
       <Footer />
     </>
